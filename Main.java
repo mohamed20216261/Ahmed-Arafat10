c46f264958c0da982303498bd8e1128e7776c33a
mohamed abdelsalam <130223240+mohamed20216261@users.noreply.github.com>
2023-05-13 16:35:02 +0300

    Add files via upload
    lab 5

--- Main.java
+++ Main.java
@@ -1,32 +1,10 @@
-import BankSystem.*;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("**** Start of Human class ****");
-        Human h = new Human("mohamed", 35, 'F');
-        h.greet();
-        h.printData();
-        System.out.println("**** End of Human class ****\n");
-
-        System.out.println("**** Start of Employee class ****");
-        Employee e = new Employee("mohamed", 35, 'F', 100, "Business");
-        e.greet();
-        e.printData();
-        System.out.println("department getter ==> " + e.getDepartment());
-        System.out.println("salary getter ==> " + e.getSalary());
-        System.out.println("**** End of Employee class ****\n");
-
-        System.out.println("**** Start of BankAccount class ****");
-        BankAccount b = new BankAccount();
-        b.setBalance(150000,15);
-        b.setBalance(125545);
-        System.out.println("balance getter ==> " + b.getBalance());
-        System.out.println("**** End of BankAccount class ****\n");
-
-        System.out.println("**** Start of Customer class ****");
-        Customer c = new Customer("Ezzat", 88, 'M', "456218945112", "12 xx st.");
-        c.greet();
-        c.printData();
-        System.out.println("**** End of Customer class ****\n");
+        BankAccount object1 = new BankAccount();
+        object1.deposit(15648);
+        object1.withdraw(1265);
+        BankAccount object2 = new BankAccount(1000, "mohamed""Bank of Egypt");
+        object2.deposit(153);
+        object2.withdraw(598);
     }
 }
